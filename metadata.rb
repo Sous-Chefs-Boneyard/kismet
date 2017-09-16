1c8ee3ae2d850c3413dcc99c4e5af5bb8eeba667
Sliim <sliim@mailoo.org>
2017-09-17 00:21:07 +0200

    Fix foodcritic (license metadata)

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'kismet'
 maintainer       'Tim Smith'
 maintainer_email 'tsmith84@gmail.com'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs/Configures kismet'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.3.0'
