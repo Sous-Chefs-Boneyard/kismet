8bd320d54aa4ef7c522b954a42424738bb3714f7
Dan Webb <dan.webb@damacus.io>
2019-08-19 20:40:54 +0100

    Auto correct using cookstyle

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: kismet
+# Cookbook:: kismet
 # Attributes:: default
 #
-# Copyright 2013, Tim Smith - tsmith84@gmail.com
+# Copyright:: 2013, Tim Smith - tsmith84@gmail.com
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
